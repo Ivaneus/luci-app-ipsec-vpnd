342e0e0eeca917e4ff0002c3346efd7c2a9fed2e
Ivaneus <908221936@qq.com>
2023-05-27 13:06:57 +0800

    Update ipsec-server.lua

--- luasrc/model/cbi/ipsec-server.lua
+++ luasrc/model/cbi/ipsec-server.lua
@@ -1,6 +1,6 @@
 mp = Map("ipsec")
 mp.title = translate("IPSec VPN Server")
-mp.description = translate("IPSec VPN connectivity using the native built-in VPN Client on iOS or Andriod (IKEv2 PSK & IKEv1 Xauth PSK)")
+mp.description = translate("IPSec VPN connectivity using the native built-in VPN Client on iOS or Andriod (IKEv2 PSK & IKEv1 Xauth PSK)<br />IKEv2 Client Mention:<br />Android Client Plsease set IPsec Identifier with psk")
 
 mp:section(SimpleSection).template  = "ipsec/ipsec_status"
 
